a230a4549c38d96ca539a3ef73282621dde87a09
mauricio-gallego <sskkaa@gmail.com>
2022-04-05 17:08:03 -0500

    update security group

--- 1. crear instancia ec2/main.tf
+++ 1. crear instancia ec2/main.tf
@@ -18,20 +18,17 @@ variable "tipo-instancia" {
   default = "t2.nano"
 }
 
-resource "aws_volume_attachment" "ebs_att" {
-  device_name = "/dev/sdh"
-  volume_id   = aws_ebs_volume.ebsexample.id
-  instance_id = aws_instance.app_server.id
-}
 
 resource "aws_instance" "app_server" {
   ami               = "ami-07eaf2ea4b73a54f6"
   instance_type     = var.tipo-instancia
   availability_zone = "us-east-1a"
+  vpc_security_group_ids   = [aws_security_group.allow_ssh.id]
+
  
 
   tags = {
-    Name = "ExampleAppServerInstancee"
+    Name = "AppServerInstancee"
   }
 }
 
@@ -40,15 +37,34 @@ resource "aws_eip" "lb" {
   vpc      = true
 }
 
-resource "aws_ebs_volume" "ebsexample" {
-  availability_zone = "us-east-1a"
-  size              = 10
-
-  tags = {
-    Name = "HelloWorld"
-  }
-
- }
  output "ip-servidor" {
     value = aws_instance.app_server.public_ip
   }
+
+  resource "aws_security_group" "allow_ssh" {
+  name        = "ssl"
+  description = "Allow ssh inbound traffic"
+  
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+    
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
